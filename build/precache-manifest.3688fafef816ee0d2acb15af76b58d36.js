cfa8142b5f7aad35616ff9acaf201a82f57c3c7f
ocfranz <franz.ogosi@usil.pe>
2020-10-15 20:08:42 -0500

    release

---
+++ build/precache-manifest.3688fafef816ee0d2acb15af76b58d36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c7699491ec944228c61ee10209a5d65",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f00bbbca7ad8d162325e",
+    "url": "/static/css/main.1db19254.chunk.css"
+  },
+  {
+    "revision": "6b5d091394172041ffef",
+    "url": "/static/js/2.6a35d9c2.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.6a35d9c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f00bbbca7ad8d162325e",
+    "url": "/static/js/main.10fac2bb.chunk.js"
+  },
+  {
+    "revision": "84bab2dc774588d3c9b9",
+    "url": "/static/js/runtime-main.5108343c.js"
+  }
+]);
